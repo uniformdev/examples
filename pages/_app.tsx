10449a7a1e7bdb3b96ed9898dc7d9eb801598cdf
Matthew Neil <matthew.neil84@gmail.com>
2022-03-09 11:09:17 +0000

    chore: remove env code block

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
-import { UniformContext } from '@uniformdev/context-react';
-import { type UniformAppProps } from '@uniformdev/context-next';
-import { createUniformContext } from '../uniformContext';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import '../styles/style.css';
+import { UniformContext } from "@uniformdev/context-react";
+import { type UniformAppProps } from "@uniformdev/context-next";
+import { createUniformContext } from "../uniformContext";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "../styles/style.css";
 
 import { ToggleEmbeddedContextDevTools } from "@uniformdev/context-devtools";
 
-function MyApp({ Component, pageProps, serverUniformContext }: UniformAppProps) {
+function MyApp({
+  Component,
+  pageProps,
+  serverUniformContext,
+}: UniformAppProps) {
   return (
     <UniformContext context={serverUniformContext || createUniformContext()}>
       <div className="leading-normal tracking-normal text-white gradient">
         <Navbar />
-        {process.env.UNIFORM_PROJECT_KEY}
         <Component {...pageProps} />
         <Footer />
       </div>
