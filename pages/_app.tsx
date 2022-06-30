44a959860febd3864c3d777724ce0e2bb0134a2a
Alex Shyba <57730703+uniform-demo@users.noreply.github.com>
2022-06-29 17:03:38 -0700

    disabling dev tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,6 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/style.css";
 
-import { ToggleEmbeddedContextDevTools } from "@uniformdev/context-devtools";
-
 const clientContext = createUniformContext();
 
 function MyApp({
@@ -16,19 +14,13 @@ function MyApp({
 }: UniformAppProps) {
   return (
     // IMPORTANT: needed to wrap the app in UniformContext
-    <UniformContext context={serverUniformContext ?? clientContext} outputType="edge">
-      <>
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-      </>
-      <ToggleEmbeddedContextDevTools
-        initialSettings={{
-          apiHost: "https://uniform.app",
-          apiKey: process.env.UNIFORM_API_KEY,
-          projectId: process.env.UNIFORM_PROJECT_ID,
-        }}
-      />
+    <UniformContext
+      context={serverUniformContext ?? clientContext}
+      outputType="edge"
+    >
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
     </UniformContext>
   );
 }
