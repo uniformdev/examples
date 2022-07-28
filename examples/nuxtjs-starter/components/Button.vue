444071af88e2b1704f23512b9e68618889f641b7
Ilyas Amezouar <ilyasa@uniform.dev>
2022-07-28 12:58:11 +0200

    feat: nuxtjs-starter

---
+++ examples/nuxtjs-starter/components/Button.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+defineProps<{ active?: boolean }>();
+</script>
+
+<template>
+  <button
+    type="button"
+    :class="{
+      button: true,
+      'button--active': active,
+    }"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  background-image: linear-gradient(
+    to right,
+    #83c6e1 0%,
+    #438fd5 51%,
+    #83c6e1 100%
+  );
+  margin: 10px;
+  padding: 15px 30px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: background-position 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+  display: block;
+}
+
+.button:hover {
+  background-position: right center;
+}
+
+.button--active {
+  background-image: linear-gradient(
+    to right,
+    #d31027 0%,
+    #ea384d 51%,
+    #d31027 100%
+  );
+}
+</style>
---
+++ examples/nuxtjs-starter/pages/[[slug]].vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import {
+  CANVAS_DRAFT_STATE,
+  CANVAS_PUBLISHED_STATE,
+  CompositionGetListResponse,
+} from "@uniformdev/canvas";
+
+const { slug: slugWithoutSlash } = useRoute().params;
+
+const slug = `/${slugWithoutSlash}`;
+
+const { $useComposition, $uniformCanvasClient, $preview } = useNuxtApp();
+const { data } = await $useComposition({ slug });
+
+const { compositions }: CompositionGetListResponse =
+  await $uniformCanvasClient.getCompositionList({
+    skipEnhance: true,
+    state: $preview ? CANVAS_DRAFT_STATE : CANVAS_PUBLISHED_STATE,
+  });
+
+const navLinks = compositions
+  .filter((c) => c.composition._slug)
+  .map((c) => {
+    return {
+      title: c.composition._name,
+      url: c.composition._slug,
+    };
+  });
+</script>
+
+<template>
+  <PageComposition :composition="data.composition" :navLinks="navLinks" />
+</template>
