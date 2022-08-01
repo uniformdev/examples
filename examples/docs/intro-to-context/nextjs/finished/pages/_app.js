36423f7519549fe5e61d833a733a1082e511afec
Ilyas Amezouar <ilyasa@uniform.dev>
2022-08-01 14:59:59 +0200

    fix(nextjs): Enable Context to the into-to-context finished example

--- examples/docs/intro-to-context/nextjs/finished/pages/_app.js
+++ examples/docs/intro-to-context/nextjs/finished/pages/_app.js
@@ -1,8 +1,21 @@
+import { UniformContext } from "@uniformdev/context-react";
+import { Context } from "@uniformdev/context";
 import "../styles/globals.css";
 import "../styles/page.css";
 
+import manifest from "../contextManifest.json";
+
+const context = new Context({
+  manifest,
+  defaultConsent: true,
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <UniformContext context={context}>
+      <Component {...pageProps} />
+    </UniformContext>
+  );
 }
 
 export default MyApp;
