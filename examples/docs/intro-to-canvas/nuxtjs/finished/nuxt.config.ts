45fd1b44eaf2681a938913b66eba04c41aedb4d0
Adam Conn <adamc@uniform.dev>
2022-08-06 13:14:08 -0700

    docs: nuxt

--- examples/docs/intro-to-canvas/nuxtjs/finished/nuxt.config.ts
+++ examples/docs/intro-to-canvas/nuxtjs/finished/nuxt.config.ts
@@ -1,4 +1,5 @@
 import { defineNuxtConfig } from "nuxt";
+import { ManifestV2 } from "@uniformdev/context";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -8,5 +9,7 @@ export default defineNuxtConfig({
     projectId: process.env.UNIFORM_PROJECT_ID,
     readOnlyApiKey: process.env.UNIFORM_API_KEY,
     apiHost: process.env.UNIFORM_CLI_BASE_URL,
+    manifest: {} as ManifestV2,
+    defaultConsent: true,
   },
 });
