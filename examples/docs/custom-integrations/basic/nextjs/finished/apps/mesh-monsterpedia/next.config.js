6bd5059dceb1dd23969e9c0b823a659768882b45
Adam Conn <adamc@uniform.dev>
2022-09-18 03:15:39 -0700

    Docs: updated custom integration (#24)
    * docs: fixed custom integrations
    
    * docs: fixed custom integrations
    
    * docs: fixed custom integrations

--- examples/docs/custom-integrations/basic/nextjs/finished/apps/mesh-monsterpedia/next.config.js
+++ examples/docs/custom-integrations/basic/nextjs/finished/apps/mesh-monsterpedia/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false, /** Must use Babel in order for Stackblitz to work */
+  swcMinify: true,
+  experimental: {
+    esmExternals: false,
+  },
 }
 
 module.exports = nextConfig
