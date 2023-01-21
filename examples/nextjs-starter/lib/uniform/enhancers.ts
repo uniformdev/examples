f16a7d412ff00623560fd4c6dafbc66d754efed7
Alex Shyba <alex.shyba@uniform.dev>
2023-01-21 12:39:31 -0800

    chore: prepare for enhancers

--- examples/nextjs-starter/lib/uniform/enhancers.ts
+++ examples/nextjs-starter/lib/uniform/enhancers.ts
@@ -1,7 +1,15 @@
+// TODO: uncomment and bring your desired enhancer imports here
 // import { enhance, EnhancerBuilder } from "@uniformdev/canvas";
 
-export default function runEnhancers(composition) {
-  //const enhancers = new EnhancerBuilder();
-  // TODO: register your CMS specific enhancers here
-  // see docs: https://docs.uniform.app/canvas/tutorials/enhancers
+export default async function runEnhancers(composition, context) {
+  // TODO: uncomment and bring your desired enhancers here
+  // await enhance({
+  //   composition,
+  //   enhancers: new EnhancerBuilder().parameter((e) => {
+  //     if (typeof e.parameter.value === "string") {
+  //       return e.parameter.value.replace(/personalization/gi, "p13n");
+  //     }
+  //   }),
+  //   context,
+  // });
 }
--- examples/nextjs-starter/pages/api/preview.ts
+++ examples/nextjs-starter/pages/api/preview.ts
@@ -1,24 +1,11 @@
-import { enhance, EnhancerBuilder } from "@uniformdev/canvas";
 import { createPreviewHandler } from "@uniformdev/canvas-next";
+import runEnhancers from "lib/uniform/enhancers";
 import getConfig from "next/config";
 
 const handler = createPreviewHandler({
   secret: () => getConfig().serverRuntimeConfig.previewSecret,
   resolveFullPath: ({ slug }) => slug,
-  // This is a dummy enhancer, it converts "personalization" to "p13n" for the sake of the demo
-  // feel free to remove it or replace it with your own enhancer
-  enhance: (composition) =>
-    enhance({
-      composition,
-      enhancers: new EnhancerBuilder().parameter((e) => {
-        if (typeof e.parameter.value === "string") {
-          return e.parameter.value.replace(/personalization/gi, "p13n");
-        }
-      }),
-      context: {
-        preview: true,
-      },
-    }),
+  enhance: async (composition) => runEnhancers(composition, { preview: true }),
 });
 
 export default handler;
