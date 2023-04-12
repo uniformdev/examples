4663e3dcd8b97e15734cc3ec0507f540978ebcb0
Alex Shyba <alex.shyba@uniform.dev>
2023-04-11 22:01:20 -0700

    chore: added netlify plugin

--- examples/gatsby5/web/gatsby-config.ts
+++ examples/gatsby5/web/gatsby-config.ts
@@ -12,6 +12,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-source-sanity",
       options: {
