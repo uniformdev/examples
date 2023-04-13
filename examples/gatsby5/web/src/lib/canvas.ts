215188fbb1f1e5061176679198a5bb4a27b60e6d
Alex Shyba <alex.shyba@uniform.dev>
2023-04-12 20:55:11 -0700

    Gatsby Preview improvements (#73)
    * temp: remove 404 handler
    
    * temp: moving preview page
    
    * Revert "temp: remove 404 handler"
    
    This reverts commit b3508aa9f616349f5d23a5e0c271920ff3ee49ba.
    
    * feat: pulling draft compositions in preview

--- examples/gatsby5/web/src/lib/canvas.ts
+++ examples/gatsby5/web/src/lib/canvas.ts
@@ -9,7 +9,7 @@ import {
 import { CANVAS_SANITY_PARAMETER_TYPES } from "@uniformdev/canvas-sanity";
 import { sanityEnhancer } from "./sanity";
 
-export const getComposition = async (path: string) => {
+export const getComposition = async (path: string, preview: boolean) => {
   const client = new CanvasClient({
     apiKey: process.env.GATSBY_UNIFORM_API_KEY,
     projectId: process.env.GATSBY_UNIFORM_PROJECT_ID,
@@ -18,7 +18,7 @@ export const getComposition = async (path: string) => {
   const { composition } = await client.getCompositionBySlug({
     slug: !path ? "/" : path,
     state:
-      process.env.NODE_ENV === "development"
+      preview || process.env.NODE_ENV === "development"
         ? CANVAS_DRAFT_STATE
         : CANVAS_PUBLISHED_STATE,
   });
