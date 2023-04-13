23e9e37aa03ebe0f1e07975c560da6bdb893f962
Alex Shyba <alex.shyba@uniform.dev>
2023-04-12 20:42:06 -0700

    Revert "temp: remove 404 handler"
    This reverts commit b3508aa9f616349f5d23a5e0c271920ff3ee49ba.

--- examples/gatsby5/web/gatsby-ssr.tsx
+++ examples/gatsby5/web/gatsby-ssr.tsx
@@ -4,3 +4,36 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 // You can delete this file if you're not using it
+
+// // Doing something similar to Gatsby-browser for SSR data
+// import "./src/styles/global.css";
+// import { UniformContext } from "@uniformdev/context-react";
+// import {
+//   Context,
+//   enableContextDevTools,
+//   ManifestV2,
+// } from "@uniformdev/context";
+// import manifest from "./uniform-manifest.json";
+// import type { GatsbySSR } from "gatsby";
+// import * as React from "react";
+
+// const context = new Context({
+//   defaultConsent: true,
+//   plugins: [enableContextDevTools()], // Use this to enable the Chrome plugin
+//   manifest: manifest as ManifestV2,
+// });
+
+// export const wrapPageElement: GatsbySSR["wrapPageElement"] = ({ element }) => {
+//   return (
+//     <UniformContext
+//       context={context}
+//       outputType={
+//         process.env.GATSBY_UNIFORM_OUTPUT_MODE
+//           ? process.env.GATSBY_UNIFORM_OUTPUT_MODE
+//           : "standard"
+//       }
+//     >
+//       {element}
+//     </UniformContext>
+//   );
+// };
