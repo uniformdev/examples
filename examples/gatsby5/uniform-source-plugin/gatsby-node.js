8813042b994299d422c612854008ae0525906631
Alex Shyba <alexs@uniform.dev>
2023-05-16 17:20:12 -0400

    feat: switch to project map based path resolution for gatsby plugin

--- examples/gatsby5/uniform-source-plugin/gatsby-node.js
+++ examples/gatsby5/uniform-source-plugin/gatsby-node.js
@@ -1,6 +1,28 @@
 const { CanvasClient, CANVAS_DRAFT_STATE } = require("@uniformdev/canvas");
-const { enhance, EnhancerBuilder } = require("@uniformdev/canvas");
+const { ProjectMapClient } = require("@uniformdev/project-map");
 const fetch = require("node-fetch");
+
+const getProjectMapClient = () => {
+  const apiKey = process.env.UNIFORM_API_KEY;
+  const apiHost = process.env.UNIFORM_CLI_BASE_URL || "https://uniform.app";
+  const projectId = process.env.UNIFORM_PROJECT_ID;
+
+  if (!apiHost)
+    throw new Error(
+      "apiHost is not specified. Project Map client cannot be instantiated"
+    );
+
+  if (!projectId)
+    throw new Error(
+      "projectId is not specified. Project Map client cannot be instantiated"
+    );
+
+  return new ProjectMapClient({
+    apiKey,
+    apiHost,
+    projectId,
+  });
+};
 
 const canvasClient = new CanvasClient({
   apiKey: process.env.UNIFORM_API_KEY,
@@ -26,12 +48,15 @@ exports.sourceNodes = async ({
       compositions.map((c) => c.composition._slug).join(", ")
   );
 
-  compositions.forEach((c) =>
+  for (let c of compositions) {
+    const { nodes } = await getProjectMapClient().getNodes({
+      compositionId: c.composition._id,
+    });
     createNode({
       ...c,
       id: createNodeId(`Composition-${c.composition._id}`),
       name: c.composition._name,
-      slug: c.composition._slug,
+      slug: nodes?.[0].path,
       componentType: c.composition.type,
       slots: JSON.stringify(c.composition.slots),
       parameters: JSON.stringify(c.composition?.parameters),
@@ -39,7 +64,6 @@ exports.sourceNodes = async ({
         type: "Compositions",
         contentDigest: createContentDigest(c),
       },
-    })
-  );
-  return;
+    });
+  }
 };
