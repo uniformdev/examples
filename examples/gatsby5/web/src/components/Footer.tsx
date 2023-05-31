c86228df4493b053fcc738451863adca2db99e52
Alex Shyba <alex.shyba@uniform.dev>
2023-05-30 17:37:56 -0700

    temp: removing render function from components for troubleshooting

--- examples/gatsby5/web/src/components/Footer.tsx
+++ examples/gatsby5/web/src/components/Footer.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
 
 export const Footer = () => {
-  return (
-    <div className="px-5 py-2 flex text-[#fff4ec] bg-[#2d2d34] font-light text-sm mt-10">
-      <p> Joyride &copy; 2023</p>
-    </div>
-  );
+  return <h1>Footer</h1>;
 };
--- examples/gatsby5/web/src/components/Hero.tsx
+++ examples/gatsby5/web/src/components/Hero.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
-import { CTA } from "./CTA";
-import {
-  UniformText,
-  registerUniformComponent,
-} from "@uniformdev/canvas-react";
+import { registerUniformComponent } from "@uniformdev/canvas-react";
 
 type HeroProps = {
   title: string;
@@ -13,22 +9,7 @@ type HeroProps = {
   ctaLink?: string;
 };
 
-export const Hero = ({ image, ctaTitle, ctaLink }: HeroProps) => (
-  <div className="grid grid-cols-2 py-[3em]">
-    <div className="mb-5">
-      <UniformText parameterId="title" as="h1" className="text-4xl mb-5" />
-      <UniformText parameterId="description" as="p" className="mb-5" />
-      {ctaTitle && ctaLink ? (
-        <CTA title={<UniformText parameterId="ctaTitle" />} link={ctaLink} />
-      ) : null}
-    </div>
-    {image ? (
-      <div>
-        <img src={image} width="500px" />
-      </div>
-    ) : null}
-  </div>
-);
+export const Hero = ({ ctaTitle }: HeroProps) => <h1>{ctaTitle}</h1>;
 
 registerUniformComponent({
   type: "hero",
