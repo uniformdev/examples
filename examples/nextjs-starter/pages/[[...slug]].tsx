a4d523119d278818e0f6ee3adc78c2fe76009778
Alex Shyba <alex.shyba@uniform.dev>
2023-06-16 17:03:31 -0700

    chore: nextjs starter update

--- examples/nextjs-starter/pages/[[...slug]].tsx
+++ examples/nextjs-starter/pages/[[...slug]].tsx
@@ -3,6 +3,7 @@ import { withUniformGetServerSideProps } from "@uniformdev/canvas-next/route";
 import { CANVAS_DRAFT_STATE, CANVAS_PUBLISHED_STATE } from "@uniformdev/canvas";
 import { getCompositionsForNavigation } from "@/lib/uniform/canvasClient";
 
+// IMPORTANT: this starter is using SSR mode by default for simplicity. SSG mode can be enabled, please check Uniform docs here: https://docs.uniform.app/docs/guides/composition/routing#project-map-with-uniform-get-server-side-props-and-with-uniform-get-static-props
 export const getServerSideProps = withUniformGetServerSideProps({
   // fetching draft composition in dev mode for convenience
   requestOptions: {
