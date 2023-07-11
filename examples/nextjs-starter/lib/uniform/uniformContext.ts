8486c0c064130432404879c5239570512d77991e
Alex Shyba <alex.shyba@uniform.dev>
2023-07-11 08:57:56 +0200

    feat: adding GA plugin

--- examples/nextjs-starter/lib/uniform/uniformContext.ts
+++ examples/nextjs-starter/lib/uniform/uniformContext.ts
@@ -7,10 +7,15 @@ import {
 } from "@uniformdev/context";
 import { NextCookieTransitionDataStore } from "@uniformdev/context-next";
 import { NextPageContext } from "next";
+import { enableGoogleGtagAnalytics } from "@uniformdev/context-gtag";
+
 import manifest from "./contextManifest.json";
 
 export function createUniformContext(serverContext?: NextPageContext) {
+  const gaPlugin = enableGoogleGtagAnalytics({ emitAll: true });
+
   const plugins: ContextPlugin[] = [
+    gaPlugin,
     enableContextDevTools(),
     enableDebugConsoleLogDrain("debug"),
   ];
