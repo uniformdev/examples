59b749b6c1e8fea5bf4d48fc14308b4a5377613f
Alex Shyba <alex.shyba@uniform.dev>
2023-07-11 09:17:33 +0200

    fix: updating ga script loading strategy

--- examples/nextjs-starter/pages/_app.tsx
+++ examples/nextjs-starter/pages/_app.tsx
@@ -1,24 +1,29 @@
 import React from "react";
-import Script from "next/script";
 
 const App = ({ Component, pageProps }) => {
   const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', '${GA_MEASUREMENT_ID}');
-        `}
-      </Script>
-
+      {GA_MEASUREMENT_ID ? (
+        <>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+                `,
+            }}
+          />
+        </>
+      ) : null}
       <Component {...pageProps} />
     </>
   );
