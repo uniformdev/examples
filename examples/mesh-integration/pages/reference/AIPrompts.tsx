8c2332cb7efbed023124cd7d496ee683f3ce9ce5
Vitali Taleika <vitalit@uniform.dev>
2023-12-07 12:30:11 +0400

    fix: ai prompts (INT-689)

--- examples/mesh-integration/pages/reference/AIPrompts.tsx
+++ examples/mesh-integration/pages/reference/AIPrompts.tsx
@@ -1,13 +1,9 @@
-import {
-  Label,
-  Textarea,
-  useMeshLocation,
-} from "@uniformdev/mesh-sdk-react";
+import { Label, Textarea, useMeshLocation } from "@uniformdev/mesh-sdk-react";
 import type { NextPage } from "next";
 import React, { ChangeEvent } from "react";
 
 const AIPrompts: NextPage = () => {
-  const { setValue, metadata } = useMeshLocation<"aiGenerate", string>();
+  const { value, setValue, metadata } = useMeshLocation<"aiGenerate", string>();
 
   const handleResultChange = async (e: ChangeEvent<HTMLTextAreaElement>) => {
     await setValue(() => ({ newValue: e.target.value ?? "" }));
@@ -21,7 +17,7 @@ const AIPrompts: NextPage = () => {
       <Textarea
         name="result"
         label="Input a prompt result"
-        value=""
+        value={value ?? ""}
         onChange={handleResultChange}
         rows={5}
       />
