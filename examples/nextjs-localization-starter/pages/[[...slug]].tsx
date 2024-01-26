dc9c6ba48066bbd54c5dfe69f80ebf43d35237bd
Khaled Garbaya <khaledgarbaya@gmail.com>
2024-01-26 11:27:56 +0100

    Use prependLocale helper

--- examples/nextjs-localization-starter/pages/[[...slug]].tsx
+++ examples/nextjs-localization-starter/pages/[[...slug]].tsx
@@ -1,7 +1,9 @@
 import PageComposition from "@/components/PageComposition";
-import { withUniformGetServerSideProps } from "@uniformdev/canvas-next/route";
+import {
+  prependLocale,
+  withUniformGetServerSideProps,
+} from "@uniformdev/canvas-next/route";
 import { CANVAS_DRAFT_STATE, CANVAS_PUBLISHED_STATE } from "@uniformdev/canvas";
-import { getCompositionsForNavigation } from "@/lib/uniform/canvasClient";
 
 // IMPORTANT: this starter is using SSR mode by default for simplicity. SSG mode can be enabled, please check Uniform docs here: https://docs.uniform.app/docs/guides/composition/routing#project-map-with-uniform-get-server-side-props-and-with-uniform-get-static-props
 export const getServerSideProps = withUniformGetServerSideProps({
@@ -12,11 +14,7 @@ export const getServerSideProps = withUniformGetServerSideProps({
         ? CANVAS_DRAFT_STATE
         : CANVAS_PUBLISHED_STATE,
   },
-  modifyPath: (path: string, { locale, defaultLocale, locales }) => {
-    if (!locales?.length) return path
-    const slug = path === '/' ? '' : path
-    return `/${locale || defaultLocale}${slug}`
-  },
+  modifyPath: prependLocale,
   handleComposition: async (routeResponse, _context, _defaultHandler) => {
     const { composition } = routeResponse.compositionApiResponse || {};
     return {
