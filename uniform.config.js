1cfc8c01f1a7685e94c95c1c993fb1f3848e5c28
Khaled Garbaya <khaledgarbaya@gmail.com>
2024-01-31 09:32:25 +0100

    chore: Improve sync command

---
+++ uniform.config.js
@@ -0,0 +1,19 @@
+const config = {
+  serialization: {
+    entitiesConfig: {
+      category: {},
+      component: {},
+      entry: { publish: true },
+      pattern: { publish: true },
+      composition: { publish: true },
+      projectMapDefinition: {},
+      projectMapNode: {},
+      asset: {},
+      locale: {},
+    },
+    directory: "./content",
+    format: "yaml",
+  },
+};
+
+module.exports = config;
