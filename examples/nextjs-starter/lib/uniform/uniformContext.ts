2c9dcab5b12b6552d4d6c3d96334bb1cc9aef75b
Alex Shyba <alex.shyba@uniform.dev>
2024-02-06 14:26:54 -0800

    example: setting visitLifespan and cookie expiration

--- examples/nextjs-starter/lib/uniform/uniformContext.ts
+++ examples/nextjs-starter/lib/uniform/uniformContext.ts
@@ -12,6 +12,10 @@ import manifest from "./contextManifest.json";
 export default function createUniformContext(
   serverContext?: NextPageContext
 ): Context {
+  // 30 minutes
+  const sessionExpirationInSeconds = 1800;
+  const secondsInDay = 60 * 60 * 24;
+  const expires = sessionExpirationInSeconds / secondsInDay;
   const plugins: ContextPlugin[] = [
     enableContextDevTools(),
     enableDebugConsoleLogDrain("debug"),
@@ -21,8 +25,12 @@ export default function createUniformContext(
     manifest: manifest as ManifestV2,
     transitionStore: new NextCookieTransitionDataStore({
       serverContext,
+      cookieAttributes: {
+        expires,
+      },
     }),
     plugins: plugins,
+    visitLifespan: sessionExpirationInSeconds * 1000,
   });
   return context;
 }
