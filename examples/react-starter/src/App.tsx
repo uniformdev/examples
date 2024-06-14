7bbf2694e0d8a77ac14d8fc1bc8ab599de526cf1
Alex Shyba <alex.shyba@uniform.dev>
2024-06-14 12:27:26 -0700

    feat: uniform context with client-side router support

--- examples/react-starter/src/App.tsx
+++ examples/react-starter/src/App.tsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { parse } from "cookie";
 import "./App.css";
-import { Link, Route, Routes } from "react-router-dom";
+import { UniformContext, useUniformContext } from "@uniformdev/context-react";
+import { createUniformContext } from "./uniform/uniformContext";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Support from "./components/Support";
 
+const clientContext = createUniformContext();
+
 function App() {
   return (
-    <>
+    <UniformContext context={clientContext}>
       <h1>Uniform React starter</h1>
       <Container />
-    </>
+    </UniformContext>
   );
 }
 
 function Container() {
+  const { context } = useUniformContext();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (typeof document === "undefined") {
+      return;
+    }
+    // Important: updating Uniform Context on route change with the latest route and cookies so the signals can evaluate based on that
+    context.update({
+      url: new URL(
+        `${document.location.protocol}//${document.location.host}${location.pathname}${location.search}`
+      ),
+      cookies: parse(document.cookie ?? ""),
+    });
+  }, [location, context]);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
