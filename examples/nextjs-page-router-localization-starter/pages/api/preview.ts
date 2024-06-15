746163f2b073e1b255507c84b1cb9c7ebddd50e2
Alex Shyba <alex.shyba@uniform.dev>
2024-06-15 16:20:19 -0700

    chore: package bump and enabling pattern preview and new serializatio… (#163)
    * chore: package bump and enabling pattern preview and new serialization config
    
    * fix: set moduleResolution to bundler
    
    * chore: next.js version bump

--- examples/nextjs-page-router-localization-starter/pages/api/preview.ts
+++ examples/nextjs-page-router-localization-starter/pages/api/preview.ts
@@ -2,6 +2,7 @@ import { createPreviewHandler } from "@uniformdev/canvas-next";
 
 const handler = createPreviewHandler({
   secret: () => process.env.UNIFORM_PREVIEW_SECRET,
+  playgroundPath: "/playground",
 });
 
 export default handler;
