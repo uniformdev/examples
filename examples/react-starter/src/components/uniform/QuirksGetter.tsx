2eeba13a5a5f352dd3bbad2d874af0d1222d8def
Alex Shyba <alex.shyba@uniform.dev>
2024-06-20 12:04:12 -0700

    feat: adding score display

--- examples/react-starter/src/components/uniform/QuirksGetter.tsx
+++ examples/react-starter/src/components/uniform/QuirksGetter.tsx
@@ -2,6 +2,13 @@ import { useUniformContext } from "@uniformdev/context-react";
 
 export default function QuirksGetter() {
   const { context } = useUniformContext();
-
-  return <div>{JSON.stringify(context.quirks, null, 2)}</div>;
+  return (
+    <div>
+      {JSON.stringify(
+        { quirks: context.quirks, scores: context.scores },
+        null,
+        2
+      )}
+    </div>
+  );
 }
