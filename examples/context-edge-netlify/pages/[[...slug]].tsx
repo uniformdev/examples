68c5b478023a37158e289690f7eb57fea12568a5
Alex Shyba <alex.shyba@uniform.dev>
2024-10-28 21:33:10 -0700

    feat: enabled SSR mode

--- examples/context-edge-netlify/pages/[[...slug]].tsx
+++ examples/context-edge-netlify/pages/[[...slug]].tsx
@@ -1,12 +1,11 @@
-import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import { getPage, pages } from "@cms";
+import type { GetServerSideProps, NextPage } from "next";
+import { getPage } from "@cms";
 import { ComponentPage, PageProps } from "../components/ComponentPage";
 
 const Home: NextPage<PageProps> = (props: PageProps) => {
   return <ComponentPage {...props} />;
 };
-
-export const getStaticProps: GetStaticProps<any> = async (context) => {
+export const getServerSideProps: GetServerSideProps<any> = async (context) => {
   const { params } = context;
   const { slug } = params || {};
   return {
@@ -14,10 +13,6 @@ export const getStaticProps: GetStaticProps<any> = async (context) => {
       page: await getPage(slug),
     },
   };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  return { paths: Object.keys(pages), fallback: false };
 };
 
 export default Home;
