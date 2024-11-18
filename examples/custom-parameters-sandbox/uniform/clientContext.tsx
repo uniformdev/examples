c2df7e5845954a13ae19246afabb33083e45dc93
Alex Shyba <alex.shyba@uniform.dev>
2024-11-18 12:21:22 -0800

    chore: remove insights plugin to simplify

--- examples/custom-parameters-sandbox/uniform/clientContext.tsx
+++ examples/custom-parameters-sandbox/uniform/clientContext.tsx
@@ -5,11 +5,7 @@ import {
   createClientUniformContext,
   useInitUniformContext,
 } from "@uniformdev/canvas-next-rsc/component";
-import {
-  ContextPlugin,
-  enableContextDevTools,
-  enableUniformInsights,
-} from "@uniformdev/context";
+import { ContextPlugin, enableContextDevTools } from "@uniformdev/context";
 // TODO: npm install @uniformdev/context-gtag if you need to enable GA4 plugin
 // import { enableGoogleGtagAnalytics } from "@uniformdev/context-gtag";
 import { useRouter } from "next/navigation";
@@ -26,19 +22,6 @@ export const UniformClientContext: ClientContextComponent = ({ manifest }) => {
         },
       })
     );
-
-    if (process.env.NEXT_PUBLIC_UNIFORM_INSIGHTS_ENABLED === "true") {
-      console.log("Uniform Insights enabled");
-      plugins.push(
-        // running against a local endpoint, will use edge middleware to rewrite to the actual endpoint
-        enableUniformInsights({
-          endpoint: {
-            type: "proxy",
-            path: "/api/analytics",
-          },
-        })
-      );
-    }
 
     // TODO: Uncomment this to enable Google Analytics 4 plugin (after installing npm install @uniformdev/context-gtag)
     // plugins.push(enableGoogleGtagAnalytics());
