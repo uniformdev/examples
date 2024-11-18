625abc5f82676f1b98d650b378bed048d958a059
trsergey <stu@dbbsoftware.com>
2024-11-18 13:54:25 +0200

    chore: style classes for text parameters

--- examples/custom-parameters-sandbox/tailwind.config.ts
+++ examples/custom-parameters-sandbox/tailwind.config.ts
@@ -1,5 +1,15 @@
 import type { Config } from "tailwindcss";
 
+const safelist = [
+  // Text parameters
+  { pattern: /text-(xs|sm|base|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl|8xl|9xl)/, variants: ['lg', 'md'] },
+  { pattern: /font-(normal|medium|bold|extrabold)/, variants: ['lg', 'md'] },
+  { pattern: /(uppercase|lowercase|capitalize)/, variants: ['lg', 'md'] },
+  { pattern: /(underline|overline|line-through)/, variants: ['lg', 'md'] },
+  { pattern: /tracking-(tighter|tight|normal|wide|wider|widest)/, variants: ['lg', 'md'] },
+  { pattern: /text-(left|center|right)/ },
+]
+
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,6 +22,7 @@ const config: Config = {
   theme: {
     extend: {},
   },
+  safelist,
   plugins: [
     require('@tailwindcss/typography'),
   ],
