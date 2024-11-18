f21d0e9db192f60a9d87345c406bab6aff897ff1
Alex Shyba <alex.shyba@uniform.dev>
2024-11-18 15:56:38 -0800

    chore: making all image urls allowed by default

--- examples/nextjs-app-router/next.config.js
+++ examples/nextjs-app-router/next.config.js
@@ -1,6 +1,11 @@
 const { withUniformConfig } = require("@uniformdev/canvas-next-rsc/config");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "*" }],
+  },
+};
 
 module.exports = withUniformConfig(nextConfig);
--- examples/nextjs-starter/next.config.js
+++ examples/nextjs-starter/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "*" }],
+  },
 };
 
 module.exports = nextConfig;
