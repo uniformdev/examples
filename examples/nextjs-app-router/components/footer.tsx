8ec5fbaa858cfeadde1405c8fe4f12ec47abd240
Alex Shyba <alex.shyba@uniform.dev>
2024-12-06 16:52:00 -0800

    feat: header/footer

---
+++ examples/nextjs-app-router/components/footer.tsx
@@ -0,0 +1,10 @@
+import { ResolveComponentResultWithType } from "@/uniform/models";
+
+export const FooterComponent = () => {
+  return <>Footer</>;
+};
+
+export const footerMapping: ResolveComponentResultWithType = {
+  type: "footer",
+  component: FooterComponent,
+};
--- examples/nextjs-app-router/uniform/mappings.ts
+++ examples/nextjs-app-router/uniform/mappings.ts
@@ -1,3 +1,5 @@
 // components will be registered here
 export { pageMapping } from "../components/page";
 export { heroMapping } from "../components/hero";
+export { headerMapping } from "../components/header";
+export { footerMapping } from "../components/footer";
