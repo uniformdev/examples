75a7f7537f9f0bc71e838bacd205a47e20671916
Alex Shyba <alex.shyba@uniform.dev>
2025-02-05 16:15:16 -0800

    feat: SSG example

--- examples/nextjs-app-router/app/[[...path]]/page.tsx
+++ examples/nextjs-app-router/app/[[...path]]/page.tsx
@@ -1,41 +1,20 @@
-// IMPORTANT This is SSR-enabled page handler. If you are looking for the SSG-enabled page handler, please use `./page.tsx.ssg-disabled` instead.
 import {
   UniformComposition,
   PageParameters,
-  ContextUpdateTransfer,
-  createServerUniformContext,
 } from "@uniformdev/canvas-next-rsc";
 import { resolveComponent } from "@/uniform/resolve";
 import retrieveRoute from "@/uniform/l18n/localeHelper";
 
-// example component that displays current quirks from Uniform Context tracker
-// import { QuirksSetter } from "@/components/quirks-setter";
+export { generateStaticParams } from "@uniformdev/canvas-next-rsc";
 
 export default async function HomePage(props: PageParameters) {
   const route = await retrieveRoute(props);
-  const serverContext = await createServerUniformContext({
-    searchParams: await props.searchParams,
-  });
   return (
-    <>
-      {/* example component that sets quirk 'province' server-side with a value */}
-      <ContextUpdateTransfer
-        serverContext={serverContext}
-        update={{
-          quirks: {
-            province: "quebec",
-          },
-        }}
-      />
-      <UniformComposition
-        {...props}
-        route={route}
-        resolveComponent={resolveComponent}
-        serverContext={serverContext}
-        mode="server"
-      />
-      {/* 
-      {/* <QuirksSetter /> */}
-    </>
+    <UniformComposition
+      {...props}
+      route={route}
+      resolveComponent={resolveComponent}
+      mode="static"
+    />
   );
 }
