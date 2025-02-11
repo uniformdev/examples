46ba4865042c1e3183a62e7026ca1175485cf419
Alex Shyba <alex.shyba@uniform.dev>
2025-02-11 14:02:12 -0800

    feat: checking asset type and render pdf or image conditionally

--- examples/nextjs-app-router/components/hero.tsx
+++ examples/nextjs-app-router/components/hero.tsx
@@ -4,11 +4,24 @@ import {
   UniformText,
 } from "@uniformdev/canvas-next-rsc/component";
 import { ResolveComponentResultWithType } from "@/uniform/models";
+import { AssetParamValue } from "@uniformdev/canvas";
+import { flattenValues } from "@uniformdev/canvas";
+import Image from "next/image";
 
 export const HeroComponent = ({
+  image1,
   component,
   context,
 }: ComponentProps<HeroProps>) => {
+  const asset = flattenValues(image1, {
+    toSingle: true,
+  });
+
+  let isPdf = false;
+  if (asset?.mediaType === "application/pdf") {
+    isPdf = true;
+  }
+
   return (
     <>
       <UniformText
@@ -25,6 +38,20 @@ export const HeroComponent = ({
         className="description"
         placeholder="Enter hero description"
       />
+      {isPdf ? (
+        <a href={asset?.url} target="_blank">
+          {asset?.title}
+        </a>
+      ) : null}
+      {!isPdf && asset?.url ? (
+        <Image
+          src={asset.url}
+          alt={asset.title!}
+          width={asset.width as number}
+          height={asset.height as number}
+        />
+      ) : null}
+      <hr />
     </>
   );
 };
@@ -32,6 +59,7 @@ export const HeroComponent = ({
 export type HeroProps = {
   title: string;
   description: string;
+  image1: AssetParamValue;
 };
 
 export const heroMapping: ResolveComponentResultWithType = {
