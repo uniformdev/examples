8363d9cfebc4bd765b259ca0bb604ace37e24e8d
Alex Shyba <alex.shyba@uniform.dev>
2025-03-10 18:24:26 -0700

    fix: adding project id to payload

--- examples/nextjs-app-router/app/api/analytics/route.ts
+++ examples/nextjs-app-router/app/api/analytics/route.ts
@@ -1,20 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
-  if (!process.env.UNIFORM_INSIGHTS_ENDPOINT || !process.env.UNIFORM_INSIGHTS_KEY) {
+  if (!process.env.UNIFORM_INSIGHTS_ENDPOINT || !process.env.UNIFORM_INSIGHTS_KEY || !process.env.UNIFORM_PROJECT_ID) {
     throw Error('Check Uniform Insights connection settings');
   }
   const destination = new URL(process.env.UNIFORM_INSIGHTS_ENDPOINT);
   destination.pathname = '/v0/events';
   destination.searchParams.set('name', 'analytics_events');
 
-  const data = await req.json();
+  const originalBody = await req.json();
+  const body = JSON.stringify({ ...originalBody, project_id: process.env.UNIFORM_PROJECT_ID });
   const response = await fetch(destination.toString(), {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${process.env.UNIFORM_INSIGHTS_KEY}`,
     },
-    body: JSON.stringify(data),
+    body,
   });
 
   const ingestionResponse = await response.json();
