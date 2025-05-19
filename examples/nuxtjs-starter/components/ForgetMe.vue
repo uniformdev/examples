12b3b733037c74227b47c46d232eb45f154ebc57
Alex Shyba <alexs@uniform.dev>
2025-05-19 11:26:14 -0700

    feat: using new previewMode composable

--- examples/nuxtjs-starter/components/ForgetMe.vue
+++ examples/nuxtjs-starter/components/ForgetMe.vue
@@ -1,10 +1,18 @@
 <script lang="ts" setup>
+import { useUniformContextualEditingState } from "@uniformdev/canvas-vue";
+const { isContextualEditing, selectedComponentReference, previewMode } =
+  useUniformContextualEditingState();
 const { context } = useNuxtApp().$useUniformContext();
 const forgetMe = async () => {
   await context.forget(true);
 };
+console.log({ previewMode, isContextualEditing, selectedComponentReference });
 </script>
 
 <template>
-  <Button @click="forgetMe">Forget me</Button>
+  <p>Preview mode: {{ previewMode }}</p>
+ 
+  <p>isContextualEditing: {{ isContextualEditing }}</p>
+
+  <Button @click="forgetMe">Forget me {{ previewMode }}</Button>
 </template>
