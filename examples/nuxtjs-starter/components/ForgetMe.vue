e4b2ac238c6831c422e9180eb9a90003913b0f0c
Alex Shyba <alexs@uniform.dev>
2025-05-22 08:38:50 -0700

    example with useUniformContextualEditingState

--- examples/nuxtjs-starter/components/ForgetMe.vue
+++ examples/nuxtjs-starter/components/ForgetMe.vue
@@ -1,18 +1,10 @@
 <script lang="ts" setup>
-import { useUniformContextualEditingState } from "@uniformdev/canvas-vue";
-const { isContextualEditing, selectedComponentReference, previewMode } =
-  useUniformContextualEditingState();
 const { context } = useNuxtApp().$useUniformContext();
 const forgetMe = async () => {
   await context.forget(true);
 };
-console.log({ previewMode, isContextualEditing, selectedComponentReference });
 </script>
 
 <template>
-  <p>Preview mode: {{ previewMode }}</p>
- 
-  <p>isContextualEditing: {{ isContextualEditing }}</p>
-
-  <Button @click="forgetMe">Forget me {{ previewMode }}</Button>
+  <Button @click="forgetMe">Forget me</Button>
 </template>
