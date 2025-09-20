e94c0f4cbe893472805c7938d3679aee62a38735
Alex Shyba <alex.shyba@uniform.dev>
2025-09-20 12:21:23 -0700

    chore: retiring old sitecore starter

--- examples/nextjs-starter-sitecore/pages/api/preview.ts
+++
@@ -1,9 +0,0 @@
-import { createPreviewHandler } from "@uniformdev/canvas-next";
-import runEnhancers from "@/uniformlib/enhancers";
-
-const handler = createPreviewHandler({
-  secret: () => process.env.UNIFORM_PREVIEW_SECRET,
-  enhance: (composition) => runEnhancers(composition, true),
-});
-
-export default handler;
