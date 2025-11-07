3f2bf9130bd57bdeb101fe8cec2b03236a0a07e2
Artem Miroshnyk <artemm@uniform.dev>
2025-11-07 15:13:25 +0100

    chore(app-route): bumping to latest deps, env variables changes, commenting out insights by default

--- examples/nextjs-app-router/app/api/analytics/route.ts
+++ examples/nextjs-app-router/app/api/analytics/route.ts
@@ -3,8 +3,8 @@ import { createBackendInsightsProxyHandler } from '@uniformdev/insights/proxy';
 
 export async function POST(req: NextRequest) {
   const handler = createBackendInsightsProxyHandler({
-    apiHost: process.env.UNIFORM_INSIGHTS_ENDPOINT!,
-    apiKey: process.env.UNIFORM_INSIGHTS_KEY!,
+    apiHost: process.env.UNIFORM_INSIGHTS_API_URL!,
+    apiKey: process.env.UNIFORM_INSIGHTS_API_KEY!,
   });
   return handler.handleRequest(await req.text());
 }
