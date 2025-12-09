f5edcb2ddc4333df93172b54145cb841ae79b52e
Alex <alexs@uniform.dev>
2025-12-08 21:00:45 -0800

    feat: middleware with default local rewrite

--- examples/nextjs-app-router-v2/middleware.ts
+++ examples/nextjs-app-router-v2/middleware.ts
@@ -1,6 +1,28 @@
 import { uniformMiddleware } from "@uniformdev/canvas-next-rsc-v2/middleware";
 
-export default uniformMiddleware();
+// TODO: in production, use locales from your internationalization setup and retrieve default local from Uniform at build time
+const defaultLocale = "en";
+const locales = ["en"];
 
-// TODO: known issue - component pattern preview (aka playground) not working with nodejs runtime
-// export const runtime = "nodejs";
+// This is the middleware that will rewrite the request path to the default locale
+export default uniformMiddleware({
+  rewriteRequestPath: async ({ url }) => ({
+    path: formatPath(url.pathname, defaultLocale),
+  }),
+});
+
+// this is the easiest way to get the middleware working but it doesn't handle locale rewrite so you have to request the page with localhost:3000/en/...
+// export default uniformMiddleware();
+
+const formatPath = (path: string, locale?: string | null): string => {
+  if (!locale) return path;
+  if (isLocaleInPath(path)) return path;
+  return `/${locale}${path}`;
+};
+
+const isLocaleInPath = (path: string): boolean => {
+  const [firstSegment] = path.split("/").filter(Boolean);
+  return firstSegment
+    ? (locales as string[]).some((locale) => locale === firstSegment)
+    : false;
+};
