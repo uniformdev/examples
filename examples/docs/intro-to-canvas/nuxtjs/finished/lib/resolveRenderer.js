0c3931ca08fd34b58db205c2bd2629db49e6a386
Alex Shyba <alex.shyba@uniform.dev>
2025-12-12 17:50:00 -0800

    fix: security update (#304)

--- examples/docs/intro-to-canvas/nuxtjs/finished/lib/resolveRenderer.js
+++
@@ -1,9 +0,0 @@
-import Body from "../components/Body.vue";
-import { DefaultNotImplementedComponent } from "@uniformdev/canvas-vue";
-
-export default function resolveRenderer({ type }) {
-  if (type == "defaultBody") {
-    return Body;
-  }
-  return DefaultNotImplementedComponent;
-}
--- examples/klevu-starter/pages/api/preview.ts
+++
@@ -1,7 +0,0 @@
-import { createPreviewHandler } from "@uniformdev/canvas-next";
-
-const handler = createPreviewHandler({
-  secret: () => process.env.UNIFORM_PREVIEW_SECRET || '',
-});
-
-export default handler;
