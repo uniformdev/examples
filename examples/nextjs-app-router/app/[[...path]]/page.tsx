257f69e89d108a2f139d0a6bbd48c5e8bf796a91
Alex <alex.shyba@uniform.dev>
2026-01-20 13:04:21 -0800

    chore: minor cleanup

--- examples/nextjs-app-router/app/[[...path]]/page.tsx
+++ examples/nextjs-app-router/app/[[...path]]/page.tsx
@@ -1,26 +1,17 @@
-// IMPORTANT This is SSR-enabled page handler. If you are looking for the SSG-enabled page handler, please use `./page.tsx.ssg-disabled` instead.
 import {
   UniformComposition,
   PageParameters,
-  createServerUniformContext,
   retrieveRoute,
 } from "@uniformdev/canvas-next-rsc";
 import { resolveComponent } from "@/uniform/resolve";
 
-// example component that displays current quirks from Uniform Context tracker
-// import { QuirksSetter } from "@/components/quirks-setter";
-
 export default async function HomePage(props: PageParameters) {
   const route = await retrieveRoute(props);
-  const serverContext = await createServerUniformContext({
-    searchParams: await props.searchParams,
-  });
   return (
     <UniformComposition
       {...props}
       route={route}
       resolveComponent={resolveComponent}
-      serverContext={serverContext}
       mode="server"
     />
   );
