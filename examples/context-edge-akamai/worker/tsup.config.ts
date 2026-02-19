3cd93c582ed807c1fa1f8b39231ad80a262a29b8
Artem Nizelnyk <artem.nizelnik@gmail.com>
2026-02-19 14:08:51 +0200

    Add output extension configuration to tsup
    without .js extensions it builds as `.mjs` which won't be accepted by akamai

--- examples/context-edge-akamai/worker/tsup.config.ts
+++ examples/context-edge-akamai/worker/tsup.config.ts
@@ -1,12 +1,18 @@
 /* eslint-disable no-var */
-import type { Options } from 'tsup';
+import type { Options } from 'tsup'
 
 export var tsup: Options = {
-    clean: true,
-    format: ['esm'],
-    entry: ['src/main.ts'],
-    splitting: false,
-    external: ['http-request', 'create-response', 'streams', 'cookies', 'encoding'],
-    noExternal: [/@uniformdev/],
-    publicDir: './public'
-};
+  clean: true,
+  format: ['esm'],
+  entry: ['src/main.ts'],
+  splitting: false,
+  external: ['http-request', 'create-response', 'streams', 'cookies', 'encoding'],
+  noExternal: [/@uniformdev/],
+  publicDir: './public',
+  outExtension() {
+    return {
+      js: `.js`
+    }
+  }
+}
+
